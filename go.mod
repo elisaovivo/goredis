c293dee3d4a26b079e43e96452db7d17e02acbf9
elisaovivo <cjbagend@gmail.com>
2023-07-17 16:15:52 +0800

    tcp

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module go-redis
 
-go 1.17
+go 1.20
---
+++ tcp/echo.go
@@ -0,0 +1,71 @@
+package tcp
+
+import (
+	"bufio"
+	"context"
+	"go-redis/lib/logger"
+	"go-redis/lib/sync/wait"
+	"io"
+	"net"
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+type EchoHandler struct {
+	activeConn sync.Map
+	closing    atomic.Bool
+}
+
+type EchoClient struct {
+	Conn    net.Conn
+	Waiting wait.Wait
+}
+
+func MakeHandler() *EchoHandler {
+	return &EchoHandler{}
+}
+
+func (e *EchoClient) Close() error {
+	e.Waiting.WaitWithTimeout(10 * time.Second)
+	_ = e.Conn.Close()
+	return nil
+}
+
+func (handler *EchoHandler) Handle(ctx context.Context, conn net.Conn) {
+	if handler.closing.Load() {
+		_ = conn.Close()
+	}
+	client := &EchoClient{
+		Conn: conn,
+	}
+	handler.activeConn.Store(client, struct{}{})
+	reader := bufio.NewReader(conn)
+	for {
+		msg, err := reader.ReadString('\n')
+		if err != nil {
+			if err == io.EOF {
+				logger.Info("Connection close")
+				handler.activeConn.Delete(client)
+			} else {
+				logger.Warn(err)
+			}
+			return
+		}
+		client.Waiting.Add(1)
+		b := []byte(msg)
+		_, _ = conn.Write(b)
+		client.Waiting.Done()
+	}
+}
+
+func (handler *EchoHandler) Close() error {
+	logger.Info("handler shutting down")
+	handler.closing.Swap(true)
+	handler.activeConn.Range(func(key, value any) bool {
+		client := key.(*EchoClient)
+		_ = client.Conn.Close()
+		return true
+	})
+	return nil
+}
