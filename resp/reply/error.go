f714af8d497cdc2c0bd6db3784f393bb3632f410
elisaovivo <cjbagend@gmail.com>
2023-07-18 17:07:23 +0800

    parse

--- resp/reply/error.go
+++ resp/reply/error.go
@@ -30,3 +30,44 @@ func MakeArgNumErrReply(cmd string) *ArgNumErrReply {
 		Cmd: cmd,
 	}
 }
+
+type SyntaxErrReply struct{}
+
+var syntaxErrBytes = []byte("-Err syntax error\r\n")
+var theSyntaxErrReply = &SyntaxErrReply{}
+
+func MakeSyntaxErrReply() *SyntaxErrReply {
+	return theSyntaxErrReply
+}
+
+func (r *SyntaxErrReply) ToBytes() []byte {
+	return syntaxErrBytes
+}
+
+func (r *SyntaxErrReply) Error() string {
+	return "Err syntax error"
+}
+
+type WrongTypeErrReply struct{}
+
+var wrongTypeErrBytes = []byte("-WRONGTYPE Operation against a key holding the wrong kind of value\r\n")
+
+func (r *WrongTypeErrReply) ToBytes() []byte {
+	return wrongTypeErrBytes
+}
+
+func (r *WrongTypeErrReply) Error() string {
+	return "WRONGTYPE Operation against a key holding the wrong kind of value"
+}
+
+type ProtocolErrReply struct {
+	Msg string
+}
+
+func (r *ProtocolErrReply) ToBytes() []byte {
+	return []byte("-ERR Protocol error: '" + r.Msg + "'\r\n")
+}
+
+func (r *ProtocolErrReply) Error() string {
+	return "ERR Protocol error: `" + r.Msg
+}
